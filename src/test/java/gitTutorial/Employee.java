521356a4435998270411cf1322a0998e309b797c
Deepak Gupta <dkumar392252@gmail.com>
2021-10-03 18:38:15 -0400

    Edited Employyee.java

--- src/test/java/gitTutorial/Employee.java
+++ src/test/java/gitTutorial/Employee.java
@@ -2,9 +2,28 @@ package gitTutorial;
 
 public class Employee {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String name;
+	private String age;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", age=" + age + "]";
 	}
 
 }
