823aa95c13e44cc6f0dde7f1a7ba4ff024d4b401
Deepak-gupta123 <75964336+Deepak-gupta123@users.noreply.github.com>
2021-10-04 21:23:14 -0400

    Update MainClass.java

--- src/test/java/gitTutorial/MainClass.java
+++ src/test/java/gitTutorial/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 
 		System.out.println("This is in Main Class");
+		System.out.println("Edits made in Main Class through Eclipse");		
 
 	}
 
